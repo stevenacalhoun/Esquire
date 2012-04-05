4ff2b3daa079764f2513cb62e52afd38c9ba4fb3
Christian Hall <jchristianhall@me.com>
2012-04-04 22:48:39 -0500

    Group selector popover made. It's not pretty.

---
+++ feedGroups.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<?php
+    // Pull in required files and make sure the user is logged in, if not redirect ot log in
+    require_once("php/userClass.php");
+    require("php/groupClass.php");
+    require_once("php/db_setup.php");
+    session_start();
+    if (!array_key_exists('user', $_SESSION)){
+        header('Location:index.php');
+    }
+    $user = $_SESSION['user'];
+    $groupIDs = $user->getGroups();
+?>
+
+<!-- Group Selection for Feeds -->
+<script src="js/jquery.js" type="text/javascript"></script>
+<script src="js/site.js" type="text/javascript"></script>
+<div class="box" id="feedBox">
+	<?php 
+		
+		// Walk over each ID and add a group block for each one
+		if (!empty($groupIDs)){
+		    foreach($groupIDs as $groupID){
+		        $group = new groupClass($groupID);
+		        if (in_array($user->getEmail(), $group->getMembers()) or in_array($user->getEmail(), $group->getPermittedMembers())){
+		 ?>
+		     	<div class="groupBlock">
+		     		<div id="group<?php echo $group->getGroupID(); ?>" class="groupTitle">
+		     			<a href="specificGroup.php?groupID=<?php echo $group->getGroupID(); ?>"><?php echo $group->getName(); ?></a>
+		     		</div>
+		     		<div class="groupText">
+		     			<?php echo $group->getDescription(); ?>
+		     		</div>
+		     	</div>   
+		     	<?php }
+		    }
+		} ?>
+</div>
